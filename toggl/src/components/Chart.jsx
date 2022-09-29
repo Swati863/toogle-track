b91050157adfa581cfb7b8625cb02ff540546fef
Amanyadav2030 <Amanyadav919821@gmail.com>
2022-09-29 21:49:54 +0530

    landing completed

--- toggl/src/components/Chart.jsx
+++ toggl/src/components/Chart.jsx
@@ -2,30 +2,31 @@ import { HStack, Button, Image, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { BsCheckLg, BsFillCaretRightFill } from 'react-icons/bs'
 
-const Chart = ({ Img }) => {
+const Chart = ({ Img, head, para1, para2 }) => {
+
 
     return (
-        <HStack borderRadius={'1rem'} background={'#412a4c'} pt={'2rem'} pb={'4rem'} color='white'>
-            <VStack textAlign={'start'} align={'flex-start'} p={'3rem 2.5rem'} w={'65rem'} >
-                <Text fontSize={'2.4rem'} fontWeight={'600'}>Make timesheets history.</Text>
+        <HStack align={'flex-start'} borderRadius={'1rem'} background={'#412a4c'} pt={'2rem'} pb={'2rem'} color='white'>
+            <VStack textAlign={'start'} align={'flex-start'} p={'3rem 2.5rem'} w={'45rem'} >
+                <Text fontSize={'2.4rem'} fontWeight={'600'}>{head}</Text>
                 <Text position={'relative'} fontSize={'1.2rem'} w={'89%'}><BsCheckLg style={{
                     display: 'inline',
-                }} fill={'#e57cd8'} />&nbsp;&nbsp; Get the data you need without disrupting your team's workflow. Track's one-click time tracking syncs across multiple platforms.</Text>
+                }} fill={'#e57cd8'} />&nbsp;&nbsp; {para1}</Text>
                 <Text fontSize={'1.2rem'} w={'89%'}><BsCheckLg style={{
                     display: 'inline',
-                }} fill={'#e57cd8'} /> &nbsp; &nbsp;Get employee buy-in with a tool designed to empower teams with knowledge, not micromanage them.</Text>
-                <HStack>
+                }} fill={'#e57cd8'} /> &nbsp; &nbsp;{para2}</Text>
+                <HStack mt={'2.5rem !important'} >
                     <Button p='1.8rem 2.8rem' fontWeight={'700'} _hover={{
                         background: '#7e6e85'
                     }} color='white' background={'#e57cd8'} borderRadius={'3rem'} >Learn More</Button>
-                    <Text><a href="#">Or sign up for free <BsFillCaretRightFill style={{
+                    <Text fontWeight={'600'}><a href="#">Or sign up for free <BsFillCaretRightFill style={{
                         display: 'inline',
                         width: '11px',
                         height: '11px',
                     }} /></a></Text>
                 </HStack>
             </VStack>
-            <VStack>
+            <VStack w={'66%'} mt={'1.8rem !important'}>
                 <Image src={Img} w={'94%'} />
             </VStack>
         </HStack >
