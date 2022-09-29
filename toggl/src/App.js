43dd8a014c5ad0c040b894ac9a09a30e743698cc
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-09-29 21:48:02 +0530

    day-3 code

--- toggl/src/App.js
+++ toggl/src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import ForgotPassword from './components/ForgotPassword';
 import Login from './Page/Login';
 import Signup from './Page/Signup';
+import AllRoutes from './Router/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      {/* <Signup /> */}
-      <Login />
-      {/* <ForgotPassword /> */}
+      <AllRoutes />
     </div>
   );
 }
--- toggl/src/components/LoginForm.jsx
+++ toggl/src/components/LoginForm.jsx
@@ -9,16 +9,28 @@ import {
     FormErrorMessage,
     Input,
     VStack,
-    Text
+    Text,
+    InputGroup,
+    InputRightElement,
+    Center,
+
 } from "@chakra-ui/react";
 import { useUserContext } from '../Context/userContext';
+import { Link } from 'react-router-dom';
+import { BiHide, BiShow } from "react-icons/bi"
 
 const LoginForm = ({ title }) => {
 
     const [data, setData] = useState({ email: "", password: "" });
-    const { loginUser, forgotPassword, user } = useUserContext()
+    const { loginUser, forgotPassword, user, error } = useUserContext()
 
-    console.log(user)
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+
+
+    // console.log(error)
+    // console.log(user?._tokenResponse?.registered)
+
 
 
     return (
@@ -32,7 +44,7 @@ const LoginForm = ({ title }) => {
                     }}
                     onSubmit={(values) => {
                         setData(values)
-                        loginUser(values.email, values.password)
+                        loginUser(values.email, values.password);
                     }}
                 >
                     {({ handleSubmit, errors, touched }) => (
@@ -65,30 +77,39 @@ const LoginForm = ({ title }) => {
                                 </FormControl>
                                 <FormControl isInvalid={!!errors.password && touched.password}>
                                     <FormLabel htmlFor="password">Password</FormLabel>
-                                    <Field
-                                        as={Input}
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        size='lg'
-                                        border="2.5px solid black"
-                                        _hover={{ background: "#ffebd7", color: "black" }}
-                                        validate={(value) => {
-                                            let error;
+                                    <InputGroup>
+                                        <Field
+                                            as={Input}
+                                            id="password"
+                                            name="password"
+                                            type={show ? 'text' : 'password'}
+                                            size='lg'
+                                            border="2.5px solid black"
+                                            _hover={{ background: "#ffebd7", color: "black" }}
+                                            validate={(value) => {
+                                                let error;
 
-                                            if (value.length < 7) {
-                                                error = "Password must contain at least 8 characters";
-                                            }
+                                                if (value.length < 7) {
+                                                    error = "Password must contain at least 8 characters";
+                                                }
 
-                                            return error;
-                                        }}
-                                    />
+                                                return error;
+                                            }}
+                                        />
+                                        <InputRightElement width='4.5rem'  >
+                                            <Center fontSize="30px" pt={2} bg="none" onClick={handleClick}>
+                                                {show ? <BiHide color='white' /> : <BiShow color='white' bg="white" />}
+                                            </Center>
+                                        </InputRightElement>
+                                    </InputGroup>
                                     <FormErrorMessage>{errors.password}</FormErrorMessage>
                                 </FormControl>
                                 <Flex justify="right" w="100%">
-                                    <Text w="fit-content" borderBottom="1px solid #E57CD8" _hover={{ color: "#E57CD8" }}>Forgot password?</Text>
+                                    <Link to="/forgot_password">
+                                        <Text w="fit-content" borderBottom="1px solid #E57CD8" _hover={{ color: "#E57CD8" }}>Forgot password?</Text>
+                                    </Link>
                                 </Flex>
-                                <Button h="48px" type="submit" bg="#E57CD8" width="full" borderRadius="3xl">
+                                <Button h="48px" _hover={{ bg: "#640090" }} type="submit" bg="#E57CD8" width="full" borderRadius="3xl">
                                     {title}
                                 </Button>
                             </VStack>
